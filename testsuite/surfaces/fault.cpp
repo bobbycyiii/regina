274b2c1b6bf7f790e31ec9aad9ce258ff6671548
Robert C. Haraway, III <bobbycyiii@fastmail.com>
2020-10-21 17:10:02 -0500

    Implemented unit tests for essential spheres
    Implemented battery of tests for determining
    whether or not a normal surface is an essential sphere.

--- testsuite/surfaces/fault.cpp
+++ testsuite/surfaces/fault.cpp
@@ -31,6 +31,8 @@
  **************************************************************************/
 
 #include <cppunit/extensions/HelperMacros.h>
+#include "surfaces/normalcoords.h"
+#include "surfaces/normalflags.h"
 #include "surfaces/normalsurface.h"
 #include "surfaces/normalsurfaces.h"
 #include "triangulation/dim3.h"
@@ -100,10 +102,8 @@ public:
 
         tri = Example<3>::threeSphere();
         delete verifyAllSeparating(tri, "Minimal 3-sphere");
-
         tri = Example<3>::simplicialSphere();
         delete verifyAllSeparating(tri, "Pentachoron boundary 3-sphere");
-
         tri = Example<3>::ball();
         delete verifyAllSeparating(tri, "One-tetrahedron ball");
 
@@ -112,7 +112,7 @@ public:
         while (p < 1000){
             if (p % 2 != 0){
                 tri = Example<3>::lens(p,q);
-                delete verifyAllSeparating(tri, "Lens space with odd p");
+                delete verifyAllSeparating(tri, "Fibonacci lens space with odd p");
             }
             p = p + q;
             q = p - q;
@@ -120,7 +120,6 @@ public:
         
         tri = Example<3>::poincareHomologySphere();
         delete verifyAllSeparating(tri, "Poincare homology sphere");
-
         tri = Example<3>::weeks();
         delete verifyAllSeparating(tri, "Weeks-Matveev-Fomenko manifold");
 
@@ -129,16 +128,13 @@ public:
         
         tri = Example<3>::s2xs1();
         delete verifyHasNonSeparating(tri, "S2xS1");
-
         tri = Example<3>::rp2xs1();
         delete verifyHasNonSeparating(tri, "RP2xS1");
-
         tri = Example<3>::rp3rp3();
         delete verifyHasNonSeparating(tri, "RP3#RP3");
-
         tri = Example<3>::smallClosedNonOrblHyperbolic();
         delete verifyHasNonSeparating(tri, "Smallest known closed nonorientable hyperbolic");
-        
+
         p = 3;
         q = 2;
         while (p < 1000){
@@ -146,7 +142,7 @@ public:
             delete verifyHasNonSeparating(tri, "Solid torus");
             if (p % 2 == 0){
                 tri = Example<3>::lens(p,q);
-                delete verifyHasNonSeparating(tri, "Lens space with even p");
+                delete verifyHasNonSeparating(tri, "Fibonacci lens space with even p");
             }
             p = p + q;
             q = p - q;
@@ -158,31 +154,160 @@ public:
         tri = Example<3>::figureEight();
         tri->idealToFinite();
         tri->intelligentSimplify();
-        delete verifyHasNonSeparating(tri, "Figure eight");
-
+        delete verifyHasNonSeparating(tri, "Figure eight knot exterior");
         tri = Example<3>::trefoil();
         tri->idealToFinite();
         tri->intelligentSimplify();
-        delete verifyHasNonSeparating(tri, "Trefoil");
-
+        delete verifyHasNonSeparating(tri, "Trefoil knot exterior");
         tri = Example<3>::whiteheadLink();
         tri->idealToFinite();
         tri->intelligentSimplify();
         delete verifyHasNonSeparating(tri, "Whitehead link");
-        
         tri = Example<3>::gieseking();
         tri->idealToFinite();
         tri->intelligentSimplify();
         delete verifyHasNonSeparating(tri, "Gieseking manifold");
-
         tri = Example<3>::cuspedGenusTwoTorus();
         tri->idealToFinite();
         tri->intelligentSimplify();
         delete verifyHasNonSeparating(tri, "Genus two surface x I");
     }
 
+    Triangulation<3>* verifyAllFundamentalSpheresTrivial(Triangulation<3>* tri, 
+            const std::string& triName){
+        NormalSurfaces* s = NormalSurfaces::enumerate(
+            tri, regina::NS_STANDARD, regina::NS_EMBEDDED_ONLY);
+        bool found_essential_sphere = false;
+        for (unsigned i = 0; i < s->size(); ++i)
+            if (s->surface(i)->isEssentialSphere()){
+                found_essential_sphere = true;
+                break;
+            }
+        delete s;
+        if (found_essential_sphere)
+            CPPUNIT_FAIL(("The irreducible manifold " + triName +
+                          " was computed to have an essential sphere.").c_str());
+        return tri;
+    }
+
+    Triangulation<3>* verifyHasEssentialSphere(Triangulation<3>* tri, 
+            const std::string& triName){
+        NormalSurfaces* s = NormalSurfaces::enumerate(
+            tri, regina::NS_STANDARD, regina::NS_EMBEDDED_ONLY);
+        bool found_essential_sphere = false;
+        for (unsigned i = 0; i < s->size(); ++i)
+            if (s->surface(i)->isEssentialSphere()){
+                found_essential_sphere = true;
+                break;
+            }
+        delete s;
+        if (!found_essential_sphere)
+            CPPUNIT_FAIL(("No essential spheres were found in the composite manifold " +
+                          triName + ".").c_str());
+        return tri;
+    }
+
+    Triangulation<3>* verifyHasQVertexEssentialSphere(Triangulation<3>* tri, 
+            const std::string& triName){
+        NormalSurfaces* s = NormalSurfaces::enumerate(
+            tri, regina::NS_QUAD, static_cast<regina::NormalListFlags>(regina::NS_VERTEX & regina::NS_EMBEDDED_ONLY));
+        bool found_essential_sphere = false;
+        for (unsigned i = 0; i < s->size(); ++i)
+            if (s->surface(i)->isEssentialSphere()){
+                found_essential_sphere = true;
+                break;
+            }
+        delete s;
+        if (!found_essential_sphere)
+            CPPUNIT_FAIL(("No essential spheres were found in the composite manifold " +
+                          triName + ".").c_str());
+        return tri;
+    }
+
     void isEssentialSphere() {
-        CPPUNIT_FAIL("Not implemented yet");
+        Triangulation<3>* tri;
+
+        // Closed irreducible manifolds
+        tri = Example<3>::threeSphere();
+        delete verifyAllFundamentalSpheresTrivial(tri, "Minimal 3-sphere");
+        tri = Example<3>::simplicialSphere();
+        delete verifyAllFundamentalSpheresTrivial(tri, "Pentachoron boundary 3-sphere");
+        tri = Example<3>::poincareHomologySphere();
+        delete verifyAllFundamentalSpheresTrivial(tri, "Poincare homology sphere");
+        tri = Example<3>::weeks();
+        delete verifyAllFundamentalSpheresTrivial(tri, "Weeks-Matveev-Fomenko manifold");
+        int p = 3;
+        int q = 2;
+        while (p < 1000){
+            tri = Example<3>::lens(p,q);
+            delete verifyAllFundamentalSpheresTrivial(tri, "Fibonacci lens space");
+            p = p + q;
+            q = p - q;
+        }
+
+        // Bounded irreducible manifolds
+        tri = Example<3>::ball();
+        delete verifyAllFundamentalSpheresTrivial(tri, "One tetrahedron ball");
+        tri = Example<3>::cuspedGenusTwoTorus();
+        tri->idealToFinite();
+        tri->intelligentSimplify();
+        delete verifyAllFundamentalSpheresTrivial(tri, "Trivial I-bundle over genus two surface");
+        tri = Example<3>::figureEight();
+        tri->idealToFinite();
+        tri->intelligentSimplify();
+        delete verifyAllFundamentalSpheresTrivial(tri, "Figure eight knot exterior");
+        tri = Example<3>::trefoil();
+        tri->idealToFinite();
+        tri->intelligentSimplify();
+        delete verifyAllFundamentalSpheresTrivial(tri, "Trefoil knot exterior");
+        tri = Example<3>::whiteheadLink();
+        tri->idealToFinite();
+        tri->intelligentSimplify();
+        delete verifyAllFundamentalSpheresTrivial(tri, "Whitehead link exterior");
+        tri = Example<3>::gieseking();
+        tri->idealToFinite();
+        tri->intelligentSimplify();
+        delete verifyAllFundamentalSpheresTrivial(tri, "Gieseking manifold");
+        tri = Example<3>::solidKleinBottle();
+        tri->idealToFinite();
+        tri->intelligentSimplify();
+        delete verifyAllFundamentalSpheresTrivial(tri, "Solid Klein bottle");
+        p = 3;
+        q = 2;
+        while (p < 1000){
+            tri = Example<3>::lst(p,q);
+            delete verifyAllFundamentalSpheresTrivial(tri, "Solid torus");
+            tri = Example<3>::lens(p,q);
+            delete verifyAllFundamentalSpheresTrivial(tri, "Lens space");
+            p = p + q;
+            q = p - q;
+        }
+
+        // The prime reducible manifold S^2 x S^1
+        tri = Example<3>::s2xs1();
+        delete verifyHasEssentialSphere(tri, "S2xS1");
+        
+        // RP3#RP3 has a reducing sphere,
+        // but in the given triangulation, there is no fundamental reducing sphere.
+        // Instead, there are two fundamental embedded one-sided RP2s
+        // that double to reducing spheres.
+
+        tri = Example<3>::rp3rp3();
+        delete verifyAllFundamentalSpheresTrivial(tri, "RP3#RP3");
+        
+        // Reducible manifolds require larger triangulations.
+        // To reduce testing time, we use quad-vertex surfaces.
+        
+        p = 3;
+        q = 2;
+        while (p < 100){
+            tri = Example<3>::lens(p,q);
+            tri->connectedSumWith(*tri);
+            tri->intelligentSimplify();
+            delete verifyHasQVertexEssentialSphere(tri, "Lens space # itself");
+            p = p + q;
+            q = p - q;
+        }
     }
 
     void isEssentialTorus() {
